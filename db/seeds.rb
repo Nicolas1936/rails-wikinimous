3da81d4850476948eba4656372805493c4c9afe2
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-10-16 09:54:43 +0200

    Implement rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# new_article = Article.new(title: 'The meaning of life', content: "It's 42!")
+# new_article.valid?  # => should be `true`
+# new_article.save    # => should not display rollback warnings
+# Article.last
+
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  articles = Article.new(
+    title:    Faker::TvShows::Friends.character,
+    content:  Faker::TvShows::Friends.quote
+  )
+  articles.save!
+end
+puts 'Finished!'
